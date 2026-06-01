a8ab8172ec0ca71ae35cb902cd5fae78a6a2d45a
Anant643 <anantsharma6034@gmail.com>
2026-06-01 11:26:31 +0530

    Update 001_Countdown_Timer.py

--- 001_Countdown_Timer.py
+++ 001_Countdown_Timer.py
@@ -6,10 +6,11 @@ seconds = int(input("Enter seconds: "))
 
 total_seconds = hours * 3600 + minutes * 60 + seconds
 
-for current in range(total_seconds, -1, -1):
-    hrs = current // 3600
-    mins = (current % 3600) // 60
-    secs = current % 60
+for remaining_seconds in range(total_seconds, -1, -1):
+
+    hrs = remaining_seconds // 3600
+    mins = (remaining_seconds % 3600) // 60
+    secs = remaining_seconds % 60
 
     print(f"{hrs:02d}:{mins:02d}:{secs:02d}")
 
